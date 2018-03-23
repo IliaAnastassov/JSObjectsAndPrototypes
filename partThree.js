10fd1e744cd27d9d08bc547e654ccddea8dbfd60
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-03-23 18:20:22 +0200

    Update Part Three

--- partThree.js
+++ partThree.js
@@ -3,3 +3,9 @@
 // **************************************************
 
 "use strict";
+
+// Add property to Array object
+Object.defineProperty(Array.prototype, "last", { get: function () { return this[this.length - 1] } });
+
+let colors = ["red", "green", "blue"];
+display(colors.last);
