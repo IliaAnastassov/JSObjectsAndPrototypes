83bd25924d4dfbc3ca244cea945b8829fbd52c28
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-03-24 00:48:12 +0200

    Udpate part 3

--- partThree.js
+++ partThree.js
@@ -2,6 +2,11 @@
 //         PART THREE - PROTOTYPES AND INHERITANCE
 // **************************************************
 
+// Definitions:
+// - A function's prototype is the object instance that will become the prototype
+// for all objects created using this function as a constructor.
+// - An object's prototype is the object instance from which the object is inherited.
+
 "use strict";
 
 // Add property to Array object
@@ -9,3 +14,16 @@ Object.defineProperty(Array.prototype, "last", { get: function () { return this[
 
 let colors = ["red", "green", "blue"];
 display(colors.last);
+
+// Demonstrate prototypes
+function Cat(name, color) {
+    this.name = name;
+    this.color = color;
+}
+
+let bob = new Cat("Bob", "Grey");
+Cat.prototype.age = 2;
+
+display(Cat.prototype);
+display(bob.__proto__);
+display(Cat.prototype === bob.__proto__);
