45bc2bd2f657e9f467375f363fd3e6c8573e10ca
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-03-25 20:40:53 +0300

    Update Part Three

--- partThree.js
+++ partThree.js
@@ -20,10 +20,65 @@ function Cat(name, color) {
     this.name = name;
     this.color = color;
 }
-
-let bob = new Cat("Bob", "Grey");
 Cat.prototype.age = 2;
 
+let bob = new Cat("Bob", "Grey");
+let greg = new Cat("Greg", "Black");
+
+// the Cat function's prototype and bob and greg's prototypes
+// are the exact same object in memory
 display(Cat.prototype);
 display(bob.__proto__);
-display(Cat.prototype === bob.__proto__);
+display(greg.__proto__);
+display(Cat.prototype === bob.__proto__
+    && Cat.prototype === greg.__proto__
+    && bob.__proto__ === greg.__proto__);
+
+// Change Cat function's prototype to a new object
+Cat.prototype = { age: 5 };
+
+let snorki = new Cat("Snorki", "Brown");
+
+display(Cat.prototype === bob.__proto__); // False
+display(Cat.prototype === snorki.__proto__); // True
+
+// Creating prototypal inheritance chains using constructor functions
+function Animal(voice) {
+    this.voice = voice || "grunt";
+}
+Animal.prototype.speak = function () {
+    display(this.voice);
+}
+
+function Dog(name, color) {
+    Animal.call(this, "Woof");
+    this.name = name;
+    this.color = color;
+}
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+let barky = new Dog("Barky", "Brown");
+display(barky);
+
+// Creating prototypal inheritance chains using classes
+class Employee {
+    constructor(jobDescription) {
+        this.jobDescription = jobDescription || "Doing actual work";
+    }
+
+    work() {
+        display(this.jobDescription);
+    }
+}
+
+class Manager extends Employee {
+    constructor(id, name) {
+        super("Create meetings");
+        this.id = id;
+        this.name = name;
+    }
+}
+
+let john = new Manager(66, "John");
+display(john);
